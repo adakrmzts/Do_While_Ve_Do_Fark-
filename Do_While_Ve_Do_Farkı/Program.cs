6609f150591643dd2bb91270bbbce3bebe64dbbd
Alpay <Alpay@ALPAY>
2025-01-12 00:32:17 +0300

    Add project files.

---
+++ Do_While_Ve_Do_Farkı/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+int sayac = 0;
+int Limitdegeri = 0;
+
+Console.WriteLine("Bir limit değeri girer misiniz ? ");
+Limitdegeri = Convert.ToInt32(Console.ReadLine());
+
+
+while (sayac <= Limitdegeri)
+{
+    Console.WriteLine("Ben bir patikalıyım.");
+    sayac = sayac + 1;
+}
+*/
+
+int sayac = 0;
+int Limitdegeri = 0;
+
+Console.WriteLine("Bir limit değeri girer misiniz ? ");
+Limitdegeri = Convert.ToInt32(Console.ReadLine());
+
+do
+{
+    Console.WriteLine("Ben bir patikalıyım.");
+    sayac = sayac + 1;
+
+}
+ while (sayac <= Limitdegeri); 
